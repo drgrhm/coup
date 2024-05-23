7fde28203d68ab8392eb69ee3d67ea7db41ddc2f
Devon Graham <drgraham@cs.ubc.ca>
2024-05-23 05:05:54 -0700

    Update up.py

--- up.py
+++ up.py
@@ -1,11 +1,10 @@
 import math
 import numpy as np   
-from collections import deque
 
 from utils import *
 
 
-def up(env, u, delta, k0=1, epsilon_min=0, n=None):
+def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=20, doubling_condition="new"):
     """ Utilitarian Procrastination """
 
     if n is None:
@@ -15,8 +14,7 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None):
         num_instances = env.get_num_instances()
     else:
         num_instances = float('inf')
-        
-
+    
     I = dict([(i, None) for i in range(n)])
     F_hat = dict([(i, 0) for i in range(n)])
     U_hat = dict([(i, 0) for i in range(n)])
@@ -24,7 +22,6 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None):
     LCB = np.zeros(n)
     alpha = dict([(i, 1) for i in range(n)])
     k = dict([(i, k0) for i in range(n)])
-    new_k = dict([(i, True) for i in range(n)])
     
     epsilon_star = 1
     i_stars = []
@@ -35,7 +32,7 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None):
 
     m = 0
 
-    print("up: running with delta={}, epsilon_min={}, m0={}".format(delta, epsilon_min, m))
+    print("up running with {} doubling condition. delta={}, epsilon_min={}, m0={}".format(doubling_condition, delta, epsilon_min, m))
 
     while len(I) > 1 and epsilon_star > epsilon_min:
         m = m + 1
@@ -52,13 +49,19 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None):
                             "total_times": total_times, 
                             "total_times_by_config": total_times_by_config}
                 else: # targeting specific epsilon
-                    print("\nERROR: up ran out of instances at m={} before reaching epsilon={}, current epsilon={}\n".format(m, epsilon_min, epsilon_star))
+                    print("\nERROR: {} ran out of instances at m={} before reaching epsilon={}, current epsilon={}\n".format(alg_name, m, epsilon_min, epsilon_star))
                     raise IndexError
-            if new_k[i]: # k has been doubled, recompute runtimes
+
+            if doubling_condition == "new":
+                dubcond = 2 * (1 - u(k[i])) * alpha[i] <= u(k[i]) * (1 - F_hat[i] + alpha[i])
+            elif doubling_condition == "old":
+                dubcond = 2 * alpha[i] <= u(k[i]) * (1 - F_hat[i])
+
+            if dubcond: # double k, recompute runtimes
+                k[i] = 2 * k[i]
                 runtimes = [env.run(i, j, k[i]) for j in range(m)]
                 F_hat[i] = sum([1 if t < k[i] else 0 for t in runtimes]) / m
                 U_hat[i] = sum(u(t) for t in runtimes) / m
-                new_k[i] = False
             else: # do only next run and constant time updates:
                 runtime = env.run(i, m - 1, k[i])
                 F_hat[i] = ((m - 1) * F_hat[i] + (1 if runtime < k[i] else 0)) / m
@@ -76,23 +79,27 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None):
 
         i_prime = np.argmax(UCB)
         epsilon_star = UCB[i_prime] - LCB[i_star]
-        i_stars.append(i_star)
-        epsilon_stars.append(epsilon_star)        
-        num_configs_remaining.append(len(I))
-        total_times.append(env.get_time_spent_running_all() / day_in_s)
-        total_times_by_config.append([env.get_time_spent_running(i) for i in range(n)])
-
-        for i in I.keys():
-            if 2 * alpha[i] <= u(k[i]) * (1 - F_hat[i]):
-                k[i] = 2 * k[i]
-                new_k[i] = True
+        
+        if (m - 1) % save_mod == 0:  
+            i_stars.append(i_star)
+            epsilon_stars.append(epsilon_star)        
+            num_configs_remaining.append(len(I))
+            total_times.append(env.get_time_spent_running_all() / day_in_s)
+            total_times_by_config.append([env.get_time_spent_running(i) for i in range(n)])
 
         if (m < 50) or (m < 1000 and m % 10 == 0) or (m % 1000 == 0):
             print("up: done m={} runs of n={} algorithms. k_min={}, k_max={}, epsilon_star={:.4f}, total_time_so_far={:.4f}, i_star={}, max_LCB={:.4f}, max_UCB={:.4f}".format(m, len(I), k[min(k, key=k.get)], k[max(k, key=k.get)], epsilon_star, env.get_time_spent_running_all() / day_in_s, i_star, LCB[i_star], UCB[i_prime]))
-        
+    
+    i_stars.append(i_star)
+    epsilon_stars.append(epsilon_star)        
+    num_configs_remaining.append(len(I))
+    total_times.append(env.get_time_spent_running_all() / day_in_s)
+    total_times_by_config.append([env.get_time_spent_running(i) for i in range(n)])
+    
     return {"i_stars": i_stars, 
             "epsilon_stars": epsilon_stars, 
             "num_configs_remaining": num_configs_remaining, 
             "total_times": total_times, 
             "total_times_by_config": total_times_by_config}
+
 
