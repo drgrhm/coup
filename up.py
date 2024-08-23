a15c7b9288dcd2c52cf12e943f55f76957051b60
Devon Graham <drgraham@cs.ubc.ca>
2024-08-23 05:03:30 -0700

    Update up.py

--- up.py
+++ up.py
@@ -1,23 +1,20 @@
 import math
 import numpy as np   
 
-from utils import *
+from utils import day_in_s
 
 
-def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=20, doubling_condition="new"):
+def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=1, max_time=float("inf"), doubling_condition="new"):
     """ Utilitarian Procrastination """
 
     if n is None:
-        n = env.get_num_configs()
+        n = env.num_configs
 
-    if hasattr(env, 'get_num_instances'):
-        num_instances = env.get_num_instances()
-    else:
-        num_instances = float('inf')
+    num_instances = env.num_instances
     
     I = dict([(i, None) for i in range(n)])
     F_hat = dict([(i, 0) for i in range(n)])
-    U_hat = dict([(i, 0) for i in range(n)])
+    U_hat = np.zeros(n)
     UCB = np.ones(n)
     LCB = np.zeros(n)
     alpha = dict([(i, 1) for i in range(n)])
@@ -27,10 +24,12 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=20, doubling_conditi
     i_stars = []
     epsilon_stars = []
     num_configs_remaining = []
+    total_time = []
     total_times = []
-    total_times_by_config = []
 
     m = 0
+
+    i_star = 0
 
     print("up running with {} doubling condition. delta={}, epsilon_min={}, m0={}".format(doubling_condition, delta, epsilon_min, m))
 
@@ -40,16 +39,29 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=20, doubling_conditi
         for i in I.keys():
             alpha[i] = math.sqrt(math.log(4 * 2.705808 * n * m**2 * (math.log2(k[i]) + 1)**2 / delta) / 2 / m)
 
+            if env.total_time >= max_time:
+                i_stars.append(i_star)
+                epsilon_stars.append(epsilon_star)        
+                num_configs_remaining.append(len(I))
+                total_time.append(env.total_time / day_in_s)
+                total_times.append(env.total_time)
+                print("up ran out time. Returning.")
+                return {"i_stars": i_stars, 
+                        "epsilon_stars": epsilon_stars, 
+                        "num_configs_remaining": num_configs_remaining, 
+                        "total_time": total_time, 
+                        "total_times": total_times}
+
             if m >= num_instances:
                 if epsilon_min == 0: # not targeting a specific epsilon
                     print("\nWARNING up ran out of instances at m={}. returning current results\n".format(m))                    
                     return {"i_stars": i_stars, 
                             "epsilon_stars": epsilon_stars, 
                             "num_configs_remaining": num_configs_remaining, 
-                            "total_times": total_times, 
-                            "total_times_by_config": total_times_by_config}
+                            "total_time": total_time, 
+                            "total_times": total_times}
                 else: # targeting specific epsilon
-                    print("\nERROR: {} ran out of instances at m={} before reaching epsilon={}, current epsilon={}\n".format(alg_name, m, epsilon_min, epsilon_star))
+                    print("\nERROR: up ran out of instances at m={} before reaching epsilon={}, current epsilon={}\n".format(m, epsilon_min, epsilon_star))
                     raise IndexError
 
             if doubling_condition == "new":
@@ -71,6 +83,7 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=20, doubling_conditi
             LCB[i] = max(U_hat[i] - alpha[i] - u(k[i]) * (1 - F_hat[i]), LCB[i])
 
         i_star = np.argmax(LCB)
+
         for _i in list(I):
             if UCB[_i] < LCB[i_star]:
                 I.pop(_i)
@@ -84,22 +97,22 @@ def up(env, u, delta, k0=1, epsilon_min=0, n=None, save_mod=20, doubling_conditi
             i_stars.append(i_star)
             epsilon_stars.append(epsilon_star)        
             num_configs_remaining.append(len(I))
-            total_times.append(env.get_time_spent_running_all() / day_in_s)
-            total_times_by_config.append([env.get_time_spent_running(i) for i in range(n)])
+            total_time.append(env.total_time / day_in_s)
+            total_times.append(env.total_times)
 
         if (m < 50) or (m < 1000 and m % 10 == 0) or (m % 1000 == 0):
-            print("up: done m={} runs of n={} algorithms. k_min={}, k_max={}, epsilon_star={:.4f}, total_time_so_far={:.4f}, i_star={}, max_LCB={:.4f}, max_UCB={:.4f}".format(m, len(I), k[min(k, key=k.get)], k[max(k, key=k.get)], epsilon_star, env.get_time_spent_running_all() / day_in_s, i_star, LCB[i_star], UCB[i_prime]))
+            print("up: done m={} runs of n={} algorithms. k_min={}, k_max={}, epsilon_star={:.4f}, total_time_so_far={:.4f}, i_star={}, max_LCB={:.4f}, max_UCB={:.4f}".format(m, len(I), k[min(k, key=k.get)], k[max(k, key=k.get)], epsilon_star, env.total_time / day_in_s, i_star, LCB[i_star], UCB[i_prime]))
     
     i_stars.append(i_star)
     epsilon_stars.append(epsilon_star)        
     num_configs_remaining.append(len(I))
-    total_times.append(env.get_time_spent_running_all() / day_in_s)
-    total_times_by_config.append([env.get_time_spent_running(i) for i in range(n)])
+    total_time.append(env.total_time / day_in_s)
+    total_times.append(env.total_times)
     
     return {"i_stars": i_stars, 
             "epsilon_stars": epsilon_stars, 
             "num_configs_remaining": num_configs_remaining, 
-            "total_times": total_times, 
-            "total_times_by_config": total_times_by_config}
+            "total_time": total_time, 
+            "total_times": total_times}
 
 
