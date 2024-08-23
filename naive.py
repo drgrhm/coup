82ff2fd64a2fe1f79cf5a6fcfe90297159483958
Devon Graham <drgraham@cs.ubc.ca>
2024-08-23 05:03:09 -0700

    Update naive.py

--- naive.py
+++ naive.py
@@ -1,21 +1,16 @@
-import argparse
-import json
 import math
-import pickle
-import numpy as np  
 
-from environment import Environment, LBEnvironment
-from utils import *
+from utils import day_in_s
 
 
 def naive(env, u, epsilon, delta, k):
     """ Naive algorithm """
     assert u(k) < epsilon, "ERROR: captime must be large enough. k={}, u(k)={}, epsilon={}".format(k, u(k), epsilon)
     
-    n = env.get_num_configs()
+    n = env.num_configs
     m = int(2 * math.log(2 * n / delta) / (epsilon - u(k))**2) + 1
     
-    assert m < env.get_num_instances(), "ERROR: cannot do m={} runs, only {} instances. u(k)={}, epsilon={}".format(m, env.get_num_instances(), u(k), epsilon)
+    assert m < env.num_instances, "ERROR: cannot do m={} runs, only {} instances. u(k)={}, epsilon={}".format(m, env.num_instances, u(k), epsilon)
     
     print("naive: doing m={} runs of n={} algorithms at captime k={}. u(k)={}, epsilon={}".format(m, n, k, u(k), epsilon))
 
@@ -23,8 +18,8 @@ def naive(env, u, epsilon, delta, k):
     for i in range(n):
         U[i] = sum(u(env.run(i, j, k)) for j in range(m)) / m
     i_star = max(U, key=U.get)
-    print("naive: i_star={}, total_time={}".format(i_star, env.get_time_spent_running_all() / day_in_s))
+    print("naive: i_star={}, total_time={}".format(i_star, env.total_time / day_in_s))
     return {'i_star': i_star, 
             'm': m,
-            'total_time': env.get_time_spent_running_all() / day_in_s}
+            'total_time': env.total_time / day_in_s}
 
