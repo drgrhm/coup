c069afb79403fcdcd46c571cd86aa10b932f7a27
Devon Graham <drgraham@cs.ubc.ca>
2024-08-23 05:01:49 -0700

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,8 +1,7 @@
-import math
 import pickle
 import numpy as np
 
-from utils import *
+# from utils import 
 
 
 class Environment():
@@ -15,37 +14,30 @@ class Environment():
         self._max_timeout = max_timeout
 
         ## Warning: shuffling can affect cumulative values (means) due to floating point precision.
-        ## Should really shuffle ids instead of runtimes for numerical stability...
         self._runtimes = self._runtimes[:, np.random.permutation(self._num_instances)] # Randomlly shuffle instances
-        self._runtimes = self._runtimes[np.random.permutation(self._num_configs), :] # Randomlly shuffle configurations         
+        self._runtimes = self._runtimes[np.random.permutation(self._num_configs), :] # Randomlly shuffle configurations
         self.reset()
 
     def reset(self):
         self._completed = dict([(i, {}) for i in range(self._num_configs)]) 
-        self._total_time = np.zeros(self._num_configs)
-        # self._captimes_used = {}
+        self._total_times = np.zeros(self._num_configs)
 
-    def get_num_configs(self):
+    @property
+    def num_configs(self):
         return self._num_configs
 
-    def get_num_instances(self):
+    @property
+    def num_instances(self):
         return self._num_instances
+    
+    @property
+    def total_times(self):
+        return np.copy(self._total_times)
 
-    def get_max_timeout(self):
-        return self._max_timeout
+    @property
+    def total_time(self):
+        return np.sum(self._total_times)
 
-    def get_time_spent_running(self, i):
-        return self._total_time[i]
-
-    def get_time_spent_running_all(self):
-        return sum(self._total_time)
-
-    def get_total_time_per_config(self):
-        return np.copy(self._total_time)
-
-    def get_true_runtimes(self, n, m):
-        """ return the runtime of the first n configs on the first m instances """
-        return self._runtimes[:n, :m]
 
     def run(self, i, j, k):
         """ run configuration i on instance j with captime k """
@@ -56,10 +48,10 @@ class Environment():
         else:  # do the run 
             if t < k:  # completed the run
                 self._completed[i][j] = True
-                self._total_time[i] += t
+                self._total_times[i] += t
                 return t
             else:  # run capped
-                self._total_time[i] += k
+                self._total_times[i] += k
                 return k
 
 
